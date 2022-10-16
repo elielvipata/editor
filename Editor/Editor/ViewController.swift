31265e4f908192e3888aa99895eff3cac5691ec9
Eliel Vipata <44714479+elielvipata@users.noreply.github.com>
2022-10-16 15:28:56 -0400

    added text editor to the view controller

---
+++ Editor/Editor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Editor
+//
+//  Created by Eliel Kilembo on 10/16/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var editField: NSTextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+}
+
